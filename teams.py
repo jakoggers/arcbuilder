c394163a60955b3a91fb98fb21a3ed47db93f3fa
HsXd <64758116+HsXd1@users.noreply.github.com>
2025-04-24 17:05:24 -0700

    Pokepaste -> Json by jakoggers

---
+++ teams.py
@@ -0,0 +1,184 @@
+# all the pokemon in one team
+
+pokemon1 = {
+	"name": "",
+	"item": "",
+	"ability": "",
+	"level": "50",
+	"tera type": "",
+	# evs
+	"ev-hp": "0",
+	"ev-attack": "0",
+	"ev-defense": "0",
+	"ev-special attack": "0",
+	"ev-special defense": "0",
+	"ev-speed": "0",
+	# separate
+	"nature": "",
+	# ivs
+	"iv-hp": "31",
+	"iv-attack": "31",
+	"iv-defense": "31",
+	"iv-special attack": "31",
+	"iv-special defense": "31",
+	"iv-speed": "31",
+	# Moves
+	"move-1": "",
+	"move-2": "",
+	"move-3": "",
+	"move-4": "",
+}
+
+pokemon2 = {
+	"name": "",
+	"item": "",
+	"ability": "",
+	"level": "50",
+	"tera type": "",
+	# evs
+	"ev-hp": "0",
+	"ev-attack": "0",
+	"ev-defense": "0",
+	"ev-special attack": "0",
+	"ev-special defense": "0",
+	"ev-speed": "0",
+	# separate
+	"nature": "",
+	# ivs
+	"iv-hp": "31",
+	"iv-attack": "31",
+	"iv-defense": "31",
+	"iv-special attack": "31",
+	"iv-special defense": "31",
+	"iv-speed": "31",
+	# Moves
+	"move-1": "",
+	"move-2": "",
+	"move-3": "",
+	"move-4": "",
+}
+
+pokemon3 = {
+	"name": "",
+	"item": "",
+	"ability": "",
+	"level": "50",
+	"tera type": "",
+	# evs
+	"ev-hp": "0",
+	"ev-attack": "0",
+	"ev-defense": "0",
+	"ev-special attack": "0",
+	"ev-special defense": "0",
+	"ev-speed": "0",
+	# separate
+	"nature": "",
+	# ivs
+	"iv-hp": "31",
+	"iv-attack": "31",
+	"iv-defense": "31",
+	"iv-special attack": "31",
+	"iv-special defense": "31",
+	"iv-speed": "31",
+	# Moves
+	"move-1": "",
+	"move-2": "",
+	"move-3": "",
+	"move-4": "",
+}
+
+pokemon4 = {
+	"name": "",
+	"item": "",
+	"ability": "",
+	"level": "50",
+	"tera type": "",
+	# evs
+	"ev-hp": "0",
+	"ev-attack": "0",
+	"ev-defense": "0",
+	"ev-special attack": "0",
+	"ev-special defense": "0",
+	"ev-speed": "0",
+	# separate
+	"nature": "",
+	# ivs
+	"iv-hp": "31",
+	"iv-attack": "31",
+	"iv-defense": "31",
+	"iv-special attack": "31",
+	"iv-special defense": "31",
+	"iv-speed": "31",
+	# Moves
+	"move-1": "",
+	"move-2": "",
+	"move-3": "",
+	"move-4": "",
+}
+
+pokemon5 = {
+	"name": "",
+	"item": "",
+	"ability": "",
+	"level": "50",
+	"tera type": "",
+	# evs
+	"ev-hp": "0",
+	"ev-attack": "0",
+	"ev-defense": "0",
+	"ev-special attack": "0",
+	"ev-special defense": "0",
+	"ev-speed": "0",
+	# separate
+	"nature": "",
+	# ivs
+	"iv-hp": "31",
+	"iv-attack": "31",
+	"iv-defense": "31",
+	"iv-special attack": "31",
+	"iv-special defense": "31",
+	"iv-speed": "31",
+	# Moves
+	"move-1": "",
+	"move-2": "",
+	"move-3": "",
+	"move-4": "",
+}
+
+pokemon6 = {
+	"name": "",
+	"item": "",
+	"ability": "",
+	"level": "50",
+	"tera type": "",
+	# evs
+	"ev-hp": "0",
+	"ev-attack": "0",
+	"ev-defense": "0",
+	"ev-special attack": "0",
+	"ev-special defense": "0",
+	"ev-speed": "0",
+	# separate
+	"nature": "",
+	# ivs
+	"iv-hp": "31",
+	"iv-attack": "31",
+	"iv-defense": "31",
+	"iv-special attack": "31",
+	"iv-special defense": "31",
+	"iv-speed": "31",
+	# Moves
+	"move-1": "",
+	"move-2": "",
+	"move-3": "",
+	"move-4": "",
+}
+
+pokemon_team = [
+	pokemon1,
+	pokemon2,
+	pokemon3,
+	pokemon4,
+	pokemon5,
+	pokemon6
+]
